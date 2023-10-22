2208c5069c5864388b9d2e69558fb2cec09f2092
Katie M <mckeanke@msu.edu>
2023-10-21 21:23:38 -0400

    Forgot to commit these.

---
+++ Caesar/Caesar/DscPoolItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caesar
+{
+    public class DscPoolItem : CaesarObject
+    {
+        public int? DscIndex;
+        public string? Qualifier;
+
+        protected override void ReadData(CaesarReader reader, CaesarContainer container)
+        {
+            Bitflags = reader.ReadUInt16();
+            uint? idk1 = reader.ReadBitflagUInt8(ref Bitflags);
+            uint? idk2 = reader.ReadBitflagUInt8(ref Bitflags);
+            DscIndex = reader.ReadBitflagInt32(ref Bitflags);
+            Qualifier = reader.ReadBitflagStringWithReader(ref Bitflags, AbsoluteAddress);
+        }
+    }
+}
---
+++ Caesar/Caesar/Enums/PresentationTypes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caesar.Enums
+{
+    public enum PresentationTypes
+    {
+        PresScaledEnum,
+        PresBasicEnum,
+        PresInt,
+        PresUInt,
+        PresScaledDecimal,
+        PresIEEEFloat,
+        PresString,
+        PresBytes,
+    }
+}
